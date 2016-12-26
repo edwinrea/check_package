20e89295277e3e7376275ac457e0da78aa2ed550
edwinrea <edwin.rea3@gmail.com>
2016-12-26 17:03:36 -0300

    Zip de ejemplo

--- script.py
+++ script.py
@@ -1,13 +1,19 @@
-import sys,argparse,zipfile
+import sys,argparse,zipfile,uuid,shutil
+
+folderName = str(uuid.uuid4())
+workDir = '/tmp/' + folderName
+
+print (workDir)
+
 
 parser = argparse.ArgumentParser(description="Validador de paquetes")
 parser.add_argument('-p', '--package', nargs='?', help='Paquete a validar', required=True)
 args = parser.parse_args()
 
-package_param = args.package
-
-print package_param
+package_param = args.package #Obtengo el nombre del parametro
 
 zf = zipfile.ZipFile(package_param, 'r')
+zf.extractall(workDir) #Extraigo el contenido del zip en /tmp/e/
+zf.close()
 
-print zf.namelist()
+shutil.rmtree(workDir) #Borro carpeta temporall
