8c0779b15c3b41eaf29125025f8dab7fbd151e77
edwinrea <edwin.rea3@gmail.com>
2016-12-27 09:25:49 -0300

    lee el contenido del zip

---
+++ other.py
@@ -0,0 +1,9 @@
+import zipfile,sys
+
+for filename in sys.argv[1:]:
+        z = zipfile.ZipFile(file(filename))
+        print "%s:" % (filename)
+        print z.namelist()[0]
+#        for f in z.namelist():
+#                print "\t%s" % (f)
+#        print ""
